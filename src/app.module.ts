4b33059385705980a6166a64ec075bd1ed39d5d8
ericiannj <ericiannj@gmail.com>
2024-12-27 14:27:04 -0300

    feat(Spots): implement spots module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { SpotsModule } from './spots/spots.module';
 
 @Module({
   imports: [EventsModule, PrismaModule, SpotsModule],
